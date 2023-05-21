3fb6786d1881f5158386677041071d5b19ed062b
Elliot Hipski <elhip03@gmail.com>
2023-05-20 20:46:16 -0400

    name change

--- src/scripts/TodoItem.js
+++ src/scripts/TodoItem.js
@@ -1,3 +1,26 @@
 export default class TodoItem{
+
+    constructor(t, d, due, p) {
+
+        this.root = document.createElement("div")
+
+        this.title = t;
+        this.description = d;
+        this.due_date = due;
+        this.priority = p;
+
+        this.completed = false
+
+        this.root.classList.add("TodoItem")
+    }
+
+
+
+    create_item() {
+
+        const title = document.createElement("h1")
+        const description = document.createElement("p")
+    }
     
+
 }
